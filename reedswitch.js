ea1bd49c85c961a9553e93275da87660be4989a5
tincann <saabnl@gmail.com>
2015-01-08 23:33:22 +0100

    Reedswitch update, send telegram message

--- reedswitch.js
+++ reedswitch.js
@@ -2,31 +2,40 @@ var events = require('events'),
 	util = require('util'),
 	io = require('pi-gpio');
 
+var STATE = {
+	CLOSED: 0,
+	OPEN: 1
+};
 
 function ReedSwitch(pinNumber, pollInterval){
 	this.pinNumber = pinNumber;
 	this.pollInterval = pollInterval;
 	this.init();
+	this.state = STATE.CLOSED;
 }
 
-util.inherit(ReedSwitch, events.EventEmitter);
+util.inherits(ReedSwitch, events.EventEmitter);
 
 ReedSwitch.prototype.init = function() {
+	io.close(this.pinNumber);
 	io.open(this.pinNumber, "input", function(){
 		this.interval = setInterval(this.poll.bind(this), this.pollInterval);
-	});
+	}.bind(this));
 };
 
 ReedSwitch.prototype.poll = function() {
 	io.read(this.pinNumber, function(err, value){
 		if(err) throw err;
-		console.log(value);
 
 		if(value){
-			this.emit('open');
+			if(this.state === STATE.CLOSED){
+				this.state = STATE.OPEN;
+				this.emit('open');
+			}
+		}else{
+			this.state = STATE.CLOSED;
 		}
-
 	}.bind(this));
 };
 
-modules.export = ReedSwitch;
+module.exports = ReedSwitch;
