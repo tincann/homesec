020f1222eb4fe95ff00a1db4e87be6a28f27ead8
tincann <saabnl@gmail.com>
2015-07-12 17:26:02 +0200

    More refactors and more support for local debugging

--- app.js
+++ app.js
@@ -1,54 +1,44 @@
 var TelegramMQ = new (require('./lib/TelegramMQ.js')),
     config = require('./config.js'),
-    pinger = new (require('./lib/Pinger.js'))(config.get('pinger')),
     tts = new (require('./lib/TTS.js'))(),
-    ReedSwitch = require('./ReedSwitch.js');
+    ReedSwitch = require('./lib/ReedSwitch.js'),
+    Camera = require('./fswebcam-wrapper/lib/Camera.js'),
+    LocationTracker = require('./lib/LocationTracker.js'),
+    pinger = new (require('./lib/Pinger.js'))(config.get('pinger'));
 
 
-var doorSensor = new ReedSwitch(config.get('doorsensor.pin_number'), 100);
-doorSensor.init();
+//Hallmonitor
+var doorSensor = new ReedSwitch(config.get('doorsensor').pin_number, 100);
 var camera = new Camera(config.get('camera'), __dirname + config.get('snapshot_path'));
 var monitor = new (require('./lib/HallMonitor.js'))(TelegramMQ, doorSensor, camera);
-
-console.log = (function(){
-	var log = console.log;
-	return function(){
-		var date = '[' + new Date + ']';
-		log.apply(console, [date].concat(Array.prototype.slice.call(arguments)));
-	};
-})();
-
 monitor.start();
 
+//Location tracker
+var locationTracker = new LocationTracker(TelegramMQ, pinger, tts);
+locationTracker.start();
+
+//Start with a test
 setTimeout(function(){
-	doorSensor.emit('open');
+  doorSensor.emit('open');
 }, 5000);
 
 setTimeout(function(){
-	doorSensor.emit('closed');
+  doorSensor.emit('closed');
 }, 10000);
 
+
+////////////
+//Utility //
+////////////
 process.on('uncaughtException', function(err) {
   console.log('Uncaught exception: ' + err);
   console.log(err.stack);
 });
 
-pinger.on('change', function (info) {
-    var message = info.name + ': ' + info.action;
-    // console.log(message);
-    TelegramMQ.send({ type: 'message', text: message});
-
-    var hour = new Date().getHours();
-    if(hour >= 8 && hour <= 22){
-      switch(info.action){
-        case 'enter':
-          tts.sayall(info.name + ' is almost home.');
-          break;
-        case 'leave':
-          tts.sayall(info.name + ' has left the building.');
-          break;
-      }
-    }
-});
-
-pinger.start();
+console.log = (function(){
+  var log = console.log;
+  return function(){
+    var date = '[' + new Date + ']';
+    log.apply(console, [date].concat(Array.prototype.slice.call(arguments)));
+  };
+})();
--- local.js
+++ local.js
@@ -1,7 +1,10 @@
 var TelegramMQ = require('./lib/TelegramMQ.js'),
 	Camera = require('./fswebcam-wrapper/lib/Camera.js'),
     config = require('./config.js'),
-    ReedSwitch = require('./lib/ReedSwitch.js');
+    ReedSwitch = require('./lib/ReedSwitch.js'),
+    tts = new (require('./lib/TTS.js'))(),
+    LocationTracker = require('./lib/LocationTracker.js'),
+    pinger = new (require('./lib/Pinger.js'))(config.get('pinger'));
 
 console.log = (function(){
 	var log = console.log;
@@ -14,10 +17,14 @@ console.log = (function(){
 var mq = new TelegramMQ();
 var imgPath = __dirname + '/snapshots/' + new Date + '.jpg';
 
+//Hallmonitor
 var doorSensor = new ReedSwitch(config.get('doorsensor').pin_number, 100);
 var camera = new Camera(config.get('camera'), __dirname + '/' + config.get('snapshot_path'));
 var monitor = new (require('./lib/HallMonitor.js'))(mq, doorSensor, camera);
 monitor.start();
+
+//Location Tracker
+var locationTracker = new LocationTracker(mq, pinger, tts);
 
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -26,12 +33,22 @@ var rl = readline.createInterface({
 });
 
 rl.on('line', function(line){
-    switch(line){
+    var args = line.split(' ');
+    switch(args[0]){
         case 'open':
             doorSensor.emit('open');
             break;
         case 'close':
             doorSensor.emit('closed');
+            break;
+        case 'enter':
+        case 'leave':
+            if(!pinger.hostmap[args[1]]){
+                pinger.hostmap[args[1]] = false;
+                pinger.init();
+            }
+
+            pinger._checkHistory(args[1], args[0] == 'enter');
             break;
     }
 });
